8e5be6d1f8de230e5266929b4abaa3c8eae37155
BartekPog <bartek.pogod@gmail.com>
2019-10-27 00:18:04 +0200

    Task3: Add template based pdf generator

--- bonus_pdf/this_should_work.py
+++ bonus_pdf/this_should_work.py
@@ -1,12 +1,23 @@
 import jinja2
 
+import lobbed_bonus_package_linux as lbp
+
 def html_generator():
     templateLoader = jinja2.FileSystemLoader(searchpath="./")
     templateEnv = jinja2.Environment(loader=templateLoader)
-    TEMPLATE_FILE = "test_1_brochure.html"
+    TEMPLATE_FILE = "template.html"
+
+
+    flat_data = lbp.generate_json("test_1")
+    
     template = templateEnv.get_template(TEMPLATE_FILE)
-    outputText = template.render(variable="xD")  # this is where to put args to the template renderer
+    outputText = template.render(flat_name="The greatest place to live", flat_data=flat_data)  # this is where to put args to the template renderer
     return outputText
 if __name__ == "__main__":
-    print(html_generator())
+    with open("final.html", "w") as file:
+        file.write(html_generator()) 
+
+    lbp.to_pdf("final.html")
+
+    # print(html_generator())
     
