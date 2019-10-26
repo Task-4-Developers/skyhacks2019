9975d398d1c34005b550723398c396e41d0d2495
Adam Gajewski <adam.gajewski@asseco.pl>
2019-10-27 00:44:27 +0200

    change model

--- task1-predict.py
+++ task1-predict.py
@@ -15,8 +15,14 @@ def define_model():
     model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same',
                      input_shape=(512, 512, 3)))
     model.add(MaxPooling2D((2, 2)))
+    
+    model.add(Conv2D(64, (2, 2)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Flatten())
     model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(64, activation='relu', kernel_initializer='he_uniform'))
     model.add(Dense(47, activation='sigmoid'))
     # compile model
     opt = SGD(lr=0.001, momentum=0.9)
