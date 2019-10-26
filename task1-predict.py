5fbe1690ced0e01322c05931ff3f7e41225def18
Adam Gajewski <adam.gajewski@asseco.pl>
2019-10-26 22:40:10 +0200

    add prediction from ready weights

---
+++ task1-predict.py
@@ -0,0 +1,33 @@
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import MaxPooling2D
+from keras.layers import Conv2D
+from keras.layers import Flatten
+from keras.layers import SGD
+
+import numpy as np
+from PIL import Image
+
+def define_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same',
+                     input_shape=(512, 512, 3)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(47, activation='sigmoid'))
+    # compile model
+    opt = SGD(lr=0.001, momentum=0.9)
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.load_weights('weights.hdf5')
+    return model
+
+def test(path):
+    model = define_model()
+    tmp = np.array(Image.open(path))
+    
+    print(model.predict(np.array(tmp)))
+
+
